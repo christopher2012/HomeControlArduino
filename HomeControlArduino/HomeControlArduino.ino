292e4c7f430d0d7eda97636818b46591b0f443fb
K.Wizner <crisw2012@gmail.com>
2015-12-15 20:29:09 +0100

    Switching light and changing brightness functionality.

--- HomeControlArduino/HomeControlArduino.ino
+++ HomeControlArduino/HomeControlArduino.ino
@@ -1,17 +1,78 @@
+#include <SoftwareSerial.h>
+int rx = 2;
+int tx = 3;
+String message = "";
+int light = 0;
+int brightness;
+int led = 9;
 
-int out = 13;
+SoftwareSerial softSerial(rx, tx);
 
-void setup()
-{
-
-	pinMode(out, INPUT);
+void setup() {
+	pinMode(led, OUTPUT);
+	softSerial.begin(9600);
+	Serial.begin(9600);
+	Serial.write("Hello from Serial");
 
 }
 
-void loop()
-{
+void loop() {
 
-	digitalWrite(out, !digitalRead(out));
-	delay(1000);
+	if (softSerial.available() > 5) {
+		message = "";
+		char c;
+		for (int i = 0; i < 3; i++) {
+			c = softSerial.read();
+			message.concat(c);
+		}
 
+		if (message.equals("MSG")) {
+			Serial.print("Wiadomosc...");
+			Serial.println(message);
+
+			c = softSerial.read();
+			if (c == '=') {
+				c = softSerial.read();
+				switch (c) {
+				case 'L':
+					Serial.println("");
+					Serial.print("Swiatlo ");
+					c = softSerial.read();
+					if (c == '1') {
+						Serial.println("ON");
+						digitalWrite(led, HIGH);
+						light = 1;
+					} else if (c == '0') {
+						Serial.println("OFF");
+						digitalWrite(led, LOW);
+						light = 0;
+					}
+					break;
+				case 'B':
+					message = "";
+					for (int i = 0; i < 3; i++) {
+						c = softSerial.read();
+						if (!(c == 0 && message.equals("")))
+							message += c;
+					}
+
+					brightness = message.toInt();
+					if (light == 1) {
+						analogWrite(led, map(brightness, 0, 100, 0, 255));
+					}
+					Serial.print("Jasnosc: ");
+					Serial.println(brightness);
+					break;
+				}
+			}
+			while (Serial.available())
+				Serial.read();
+			//softSerial.flush();
+		}
+	}
+	else {
+		while (Serial.available())
+			Serial.read();
+		//softSerial.flush();
+	}
 }
