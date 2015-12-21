8bbcffc11e1b1594955be766aff06ceb45c81ca9
K.Wizner <crisw2012@gmail.com>
2015-12-21 19:21:38 +0100

    Add LCD and touch sensor

--- HomeControlArduino/HomeControlArduino.ino
+++ HomeControlArduino/HomeControlArduino.ino
@@ -1,22 +1,60 @@
-#include <SoftwareSerial.h>
+#include <SoftwareSerial\SoftwareSerial.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
 int rx = 2;
 int tx = 3;
 String message = "";
 int light = 0;
 int brightness;
-int led = 9;
+int led = 8;
+
+int alarm = 0;
+int alarmAddress = 0;
+int autoSwitchOffLight = 0;
+int autoSwitchOffLightAddress = 1;
+boolean touchFlag = true;
+
+LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
 
 SoftwareSerial softSerial(rx, tx);
+
+String SSID = "UPC2633536";
+String PASSWORD = "niezgadnieszmnie";
+
+#define HOST_NAME   "api.thingspeak.com"
+#define HOST_PORT   80
+#define ONE_WIRE_BUS 7
+#define TOUCH_SENSOR 7
 
 void setup() {
 	pinMode(led, OUTPUT);
 	softSerial.begin(9600);
 	Serial.begin(9600);
 	Serial.write("Hello from Serial");
-
+	lcd.begin(16, 2);
+	lcd.setCursor(0, 0);
+	lcd.print("Initialize LCD!");
+	pinMode(TOUCH_SENSOR, INPUT);
 }
 
 void loop() {
+
+	if (digitalRead(TOUCH_SENSOR) == HIGH && touchFlag) {
+		touchFlag = false;
+		Serial.println(".");
+		if (light == 0) {
+			digitalWrite(led, HIGH);
+			light = 1;
+		}
+		else{
+			digitalWrite(led, LOW);
+			light = 0;
+		}
+	}
+	else if (digitalRead(TOUCH_SENSOR) == LOW){
+		touchFlag = true;
+	}
 
 	if (softSerial.available() > 5) {
 		message = "";
@@ -65,14 +103,10 @@ void loop() {
 					break;
 				}
 			}
-			while (Serial.available())
-				Serial.read();
-			//softSerial.flush();
+			while (softSerial.available())
+				softSerial.read();
 		}
 	}
 	else {
-		while (Serial.available())
-			Serial.read();
-		//softSerial.flush();
 	}
 }
